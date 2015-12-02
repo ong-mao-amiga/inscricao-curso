503df90ada0ed66e999e26327a815966733c9a83
jcsfbass <jcsfbass@gmail.com>
2015-12-02 18:03:27 -0300

    Criando seção do formulário de inscrição

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-var resizeMain = function(){
-	home.background.resize(window.innerWidth, window.innerHeight);
-};
-
-resizeMain();
-window.addEventListener('load', resizeMain);
-window.addEventListener('resize', resizeMain);
